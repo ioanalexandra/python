bb53a365af4af76bdf115f792ec56e937d3c62be
ioanalexandra <alexandra.ioan.99@gmail.com>
2021-12-13 16:59:55 +0200

    Am adaugat o clasa application care contine toate obiectele

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyglet.window import mouse
 from pyglet import shapes
 
 window = pyglet.window.Window()
-window.set_size(400, 360)
+window.set_size(600, 300)
 pyglet.gl.glClearColor(111/255, 145/255, 47/255, 1)
 #pyglet.gl.glClearColor(173/255, 142/255, 28/255, 1)
 #pyglet.gl.glClearColor(179/255, 104/255, 12/255,1)
@@ -13,6 +13,45 @@ pyglet.gl.glClearColor(111/255, 145/255, 47/255, 1)
   #                        font_size=36,
    #                       x=window.width//2, y=window.height//2,
     #                      anchor_x='center', anchor_y='center')
+
+class Application:
+    def __init__(self):
+        self.drawables = list()
+        self.board = Board(0, window.height - 180)
+        self.tabla = [ [ 0 for i in range(3) ] for j in range(3) ]
+        self.turn = 0
+    def draw(self, p):
+        self.board.draw(p)
+        for e in self.drawables:
+            e.draw(p)
+    def putX(self, x, y):
+        if x>=self.board.x and x<=self.board.lineWidth*2 + self.board.size*3:
+            if y<=self.board.y and y>=self.board.y-(self.board.lineWidth*2 + self.board.size*3):
+                aux = self.board.size + self.board.lineWidth
+                x2 = int(x / aux)
+                y2 = int(y / aux)
+                if y2 > 2:
+                    y2 = 2
+                if x2 > 2:
+                    x2 = 2
+                if self.tabla[x2][y2] == 0:
+                    self.tabla[x2][y2] = 1
+                    self.turn = (self.turn + 1) % 2
+                    self.drawables.append(Xobj(int(x2 * aux + aux / 4), int((y2+1) * aux - aux / 4)))
+    def putO(self, x, y):
+        if x>=self.board.x and x<=self.board.lineWidth*2 + self.board.size*3:
+            if y<=self.board.y and y>=self.board.y-(self.board.lineWidth*2 + self.board.size*3):
+                aux = self.board.size + self.board.lineWidth
+                x2 = int(x / aux)
+                y2 = int(y / aux)
+                if y2 > 2:
+                    y2 = 2
+                if x2 > 2:
+                    x2 = 2
+                if self.tabla[x2][y2] == 0:
+                    self.tabla[x2][y2] = 1
+                    self.turn = (self.turn + 1) % 2
+                    self.drawables.append(Oobj(int(x2 * aux + aux / 2 - 12), int((y2+1) * aux - aux / 2 + 12)))
 
 
 
@@ -24,7 +63,7 @@ class Drawable:
         self.size = size
 
 class Xobj(Drawable):
-    def __init__(self, x, y, color = (179, 104, 12), size = 20, lineWidth = 10 ):
+    def __init__(self, x, y, color = (179, 104, 12), size = 40, lineWidth = 10 ):
         super().__init__(x, y, color, size)
         self.lineWidth = lineWidth
     def draw(self, p):
@@ -37,7 +76,7 @@ class Xobj(Drawable):
 
 class Oobj(Drawable):
     batch = pyglet.graphics.Batch()
-    def __init__(self, x, y, color = (179, 104, 12), radius = 40, secColor = (111, 145,47), lineWidth = 10):
+    def __init__(self, x, y, color = (179, 104, 12), radius = 35, secColor = (111, 145,47), lineWidth = 10):
         super().__init__(x, y, color, radius)
         self.circle1 = shapes.Circle(x, y, radius, color = self.color, batch = Oobj.batch)
         self.circle2 = shapes.Circle(x, y, radius-lineWidth%radius, color = secColor, batch = Oobj.batch)
@@ -61,23 +100,18 @@ class Board(Drawable):
         )
 
 
+app = Application()
 
-listX = list()
 
 @window.event
 def on_draw():
     window.clear()
-    Board(10, 350).draw(pyglet)
-    global listX
-    for x in listX:
-        x.draw(pyglet)
+    app.draw(pyglet)
 
 @window.event
 def on_mouse_release(x, y, button, modifiers):
         if(button == mouse.LEFT):
-            
-            global listX
-            listX.append(Xobj(x, y))
+            app.putO(x, y)
 
 
 pyglet.app.run()
